c7ddb8e120de956c4dc77a0b5a939204d1b8ae82
Maxim Joska <jomaxi@seznam.cz>
2023-12-08 09:05:17 +0100

    úprava auth routes

--- src/main/kotlin/cz/upce/bvwa2/routes/auth/AuthRoutes.kt
+++ src/main/kotlin/cz/upce/bvwa2/routes/auth/AuthRoutes.kt
@@ -1,10 +1,12 @@
 package cz.upce.bvwa2.routes.auth
 
 import cz.upce.bvwa2.auth.UserPrincipal
+import cz.upce.bvwa2.repository.UserRepository
 import io.ktor.http.*
 import io.ktor.resources.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.resources.*
 import io.ktor.server.resources.post
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -12,38 +14,51 @@ import io.ktor.server.sessions.*
 import org.kodein.di.instance
 import org.kodein.di.ktor.closestDI
 
-@Resource("/login")
-class Login
+@Resource("/auth")
+class Auth {
+    @Resource("/login")
+    class Login(val parent: Auth = Auth())
 
-@Resource("/logout")
-class Logout
+    @Resource("/logout")
+    class Logout(val parent: Auth = Auth())
 
-@Resource("/refresh")
-class Refresh
+    @Resource("/refresh")
+    class Refresh(val parent: Auth = Auth())
+
+    @Resource("/check-username-available")
+    class CheckUsernameAvailable(val parent: Auth = Auth(), val username: String)
+}
 
 fun Route.authRoutes() {
     val sessionStorage by closestDI().instance<SessionStorage>()
+    val userRepository by closestDI().instance<UserRepository>()
 
-    route("/auth") {
-        authenticate("form") {
-            post<Login> {
-                call.sessions.set(call.principal<UserPrincipal>())
+    authenticate("form") {
+        post<Auth.Login> {
+            call.sessions.set(call.principal<UserPrincipal>())
 
-                call.respond(HttpStatusCode.OK)
-            }
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+
+    authenticate("session") {
+        post<Auth.Logout> {
+            call.sessions.clear<UserPrincipal>()
+            sessionStorage.invalidate(call.sessionId!!)
+
+            call.respond(HttpStatusCode.OK)
         }
 
-        authenticate("session") {
-            post<Logout> {
-                call.sessions.clear<UserPrincipal>()
-                sessionStorage.invalidate(call.sessionId!!)
-
-                call.respond(HttpStatusCode.OK)
-            }
-
-            post<Refresh> {
-                call.respond(HttpStatusCode.OK)
-            }
+        post<Auth.Refresh> {
+            call.respond(HttpStatusCode.OK)
         }
+    }
+
+    // 0 - false, 1 - true
+    get<Auth.CheckUsernameAvailable> {
+        if (true)
+            call.respond("1")
+        else
+            call.respond("0")
     }
 }
