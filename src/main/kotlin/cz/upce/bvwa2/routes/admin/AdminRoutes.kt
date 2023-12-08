93672141ef06ab820b408d19fd3b68c264ffd14e
Maxim Joska <jomaxi@seznam.cz>
2023-12-08 09:04:43 +0100

    přidání základních routes pro /admin

--- src/main/kotlin/cz/upce/bvwa2/routes/admin/AdminRoutes.kt
+++ src/main/kotlin/cz/upce/bvwa2/routes/admin/AdminRoutes.kt
@@ -1,6 +1,12 @@
 package cz.upce.bvwa2.routes.admin
 
+import io.github.omkartenkale.ktor_role_based_auth.withRole
+import io.ktor.http.*
 import io.ktor.resources.*
+import io.ktor.server.application.*
+import io.ktor.server.resources.*
+import io.ktor.server.resources.put
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 @Resource("/admin")
@@ -8,13 +14,33 @@ class Admin {
     @Resource("/user/{id}")
     data class User(val parent: Admin = Admin(), val id: String = "") {
         @Resource("/role")
-        data class Role(val parent: User = User())
+        data class Role(val parent: User = User(), val newRole: String)
     }
 
     @Resource("/users")
     data class Users(val parent: Admin = Admin())
 }
 
-fun Route.audminRoutes() {
+fun Route.adminRoutes() {
+    withRole("admin") {
+        put<Admin.User.Role> {
+            call.respond(HttpStatusCode.OK)
+        }
 
+        get<Admin.User> {
+            call.respond(HttpStatusCode.OK)
+        }
+
+        put<Admin.User> {
+            call.respond(HttpStatusCode.OK)
+        }
+
+        delete<Admin.User> {
+            call.respond(HttpStatusCode.OK)
+        }
+
+        get<Admin.Users> {
+            call.respond(HttpStatusCode.OK)
+        }
+    }
 }
