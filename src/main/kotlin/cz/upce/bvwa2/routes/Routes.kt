9242daf563467c894b6f7626d6e2f9f704c39fe8
Maxim Joska <jomaxi@seznam.cz>
2023-12-12 15:57:05 +0100

    přidaná reactu

--- src/main/kotlin/cz/upce/bvwa2/routes/Routes.kt
+++ src/main/kotlin/cz/upce/bvwa2/routes/Routes.kt
@@ -11,9 +11,9 @@ import io.ktor.server.routing.*
 
 fun Application.configureRoutes() {
     routing {
-//        singlePageApplication {
-//            react("react-app")
-//        }
+        singlePageApplication {
+            react("fe/build")
+        }
 
         authRoutes()
         userRoutes()
